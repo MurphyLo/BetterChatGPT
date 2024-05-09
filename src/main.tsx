2aa896f3a5fe0c1a1f4778a6a1332654e5bf83eb
MurphyLo <69335326+MurphyLo@users.noreply.github.com>
2024-05-09 11:58:20 +0800

    Update main.tsx
    import BrowserRouter from react-router-dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom'; // 引入Router
 import App from './App';
 import './main.css';
 await import('katex/dist/katex.min.css');
@@ -8,6 +9,8 @@ import './i18n';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>
 );
